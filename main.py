927581c1b9e6541c9105b1d514b8e44bb37872e1
Danny Diaz <meliodasfistery@gmail.com>
2024-08-18 21:34:50 -0500

    Versión con una clase (abstracción y encapsulamiento + patron de diseño Singleton

--- main.py
+++ main.py
@@ -1,33 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import pandas as pd
+from models.prestamo import PrestamoManager
 
 app = Flask(__name__)
+
+prestamo_manager = PrestamoManager.get_instance()
 
 df = pd.read_csv("./data/prestamos.csv")
 # df_records = df.to_dict(orient='records')
 
 @app.route('/')
 def index():
-
-    # Limpiando
-    df['Fecha de préstamo'] = pd.to_datetime(df['Fecha de préstamo'])
-    df['Fecha de devolución'] = pd.to_datetime(df['Fecha de devolución'])
-
-    # Calcular estadísticas descriptivas
-    df['Días de préstamo'] = (df['Fecha de devolución'] - df['Fecha de préstamo']).dt.days
-    max_dias_prestamo = df['Días de préstamo'].max()
-    promedio_dias_prestamo = df['Días de préstamo'].mean()
-
     return render_template('index.html', 
-                            registros=len(df), 
-                            max_dias_prestamo=max_dias_prestamo, 
-                            promedio_dias_prestamo=promedio_dias_prestamo)
+                           registros=len(prestamo_manager.dataframe), 
+                           max_dias_prestamo=prestamo_manager.max_dias_prestamo(), 
+                           promedio_dias_prestamo=prestamo_manager.promedio_dias_prestamo())
 
-# Ruta para mostrar la tabla completa
 @app.route('/historial')
 def show_table():
-    # Recuperar el DataFrame de la sesión
-    return render_template('historial.html', table=df.to_html(index=False))
+    return render_template('historial.html', table=prestamo_manager.dataframe.to_html(index=False))
 
 if __name__ == '__main__':
     app.run(debug=True)
