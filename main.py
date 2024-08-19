291d2cf2b2274a61aaf1471eb6ed16a07685f8d2
Danny Diaz <meliodasfistery@gmail.com>
2024-08-18 21:36:06 -0500

    Codestyle

--- main.py
+++ main.py
@@ -9,16 +9,23 @@ prestamo_manager = PrestamoManager.get_instance()
 df = pd.read_csv("./data/prestamos.csv")
 # df_records = df.to_dict(orient='records')
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html', 
-                           registros=len(prestamo_manager.dataframe), 
-                           max_dias_prestamo=prestamo_manager.max_dias_prestamo(), 
-                           promedio_dias_prestamo=prestamo_manager.promedio_dias_prestamo())
+    return render_template(
+        "index.html",
+        registros=len(prestamo_manager.dataframe),
+        max_dias_prestamo=prestamo_manager.max_dias_prestamo(),
+        promedio_dias_prestamo=prestamo_manager.promedio_dias_prestamo(),
+    )
 
-@app.route('/historial')
+
+@app.route("/historial")
 def show_table():
-    return render_template('historial.html', table=prestamo_manager.dataframe.to_html(index=False))
+    return render_template(
+        "historial.html", table=prestamo_manager.dataframe.to_html(index=False)
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
